b5c50de4e712c52b12b46bfcd011d65993dd3dd1
Matthew Tang <38430052+Matt-Tang@users.noreply.github.com>
2019-01-03 13:57:17 -0500

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,37 @@
+﻿### Variable declaration
+$path = Split-Path $MyInvocation.MyCommand.Path
+
+$share = "\\CL11180.sunlifecorp.com\JavaDiscovery"
+$scriptPath = ($path + "\process.ps1")
+$textPath =  ($path + "\keywords.txt")
+
+$storage = "C:\JavaProcess"
+
+if (Test-Path $share)
+{
+    if (Test-Path $storage){
+          Remove-Item -Force -Recurse -Path $storage
+    }
+
+    New-Item -Path $storage -ItemType directory | Out-Null
+    
+    Move-Item -Path "$scriptPath" -Destination $storage | Out-Null
+    Move-Item -Path "$textPath" -Destination $storage | Out-Null
+	
+	Write-Host "Process Script running..."
+    & "C:\JavaProcess\process.ps1"
+	Write-Host "Process Script done"
+    Start-Sleep -s 2
+    Write-Host "Task script running..."
+    & "$path\task.ps1"
+	Write-Host "Task script done"
+	
+  
+}
+else
+{
+    Write-Host "Share folder not available, operation not successful. . ."
+}
+
+
+
---
+++ task.ps1
@@ -0,0 +1,27 @@
+﻿$TaskName = "Java_Process"
+$TaskDescription = "Find Java Processes"
+$TaskCommand = "c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe"
+$TaskScript = "C:\JavaProcess\process.ps1"
+$TaskArg = "-WindowStyle Hidden -NonInteractive -Executionpolicy unrestricted -file $TaskScript"
+$TaskStartTime = [datetime]::Now.AddMinutes(5)
+$service = new-object -ComObject("Schedule.Service")
+$service.Connect()
+$rootFolder = $service.GetFolder("\")
+$TaskDefinition = $service.NewTask(0)
+$TaskDefinition.RegistrationInfo.Description = "$TaskDescription"
+$TaskDefinition.Settings.Enabled = $true
+$TaskDefinition.Settings.AllowDemandStart = $true
+
+$triggers = $TaskDefinition.Triggers
+$trigger = $triggers.Create(3)
+$trigger.StartBoundary = $TaskStartTime.ToString("yyyy-MM-dd'T'HH:mm:ss")
+$trigger.DaysOfWeek = 2
+$trigger.Repetition.Interval = "PT1H"
+$trigger.Repetition.Duration = "P1D"
+$trigger.Enabled = $true
+
+$Action = $TaskDefinition.Actions.Create(0)
+$action.Path = "$TaskCommand"
+$action.Arguments = "$TaskArg"
+
+$rootFolder.RegisterTaskDefinition("$TaskName",$TaskDefinition,6,"System",$null,5)
